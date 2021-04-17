e7f0eed9fdab209e45b4d02a90d17831da71ebbd
rizqito <qito25@gmail.com>
2021-04-17 17:29:12 +0800

    no.3

--- 3.php
+++ 3.php
@@ -1,12 +1,13 @@
 <?php
+	$kata = array("D","U","M","B","W","A","Y","S","I","D","U","J","I","A","N");
 	$star = 5;
 	for($a = 1; $a <= $star; $a++){
-	for($b = $star; $b >= $a; $b -= 1){
-		echo "&nbsp";
-	}
-	for($c = 1; $c <= $a; $c++){
-		echo $c;
-	}
+		for($b = $star; $b >= $a; $b -= 1){
+			echo "&nbsp";
+		}
+		for($c = 1; $c <= $a; $c++){
+			echo $c;
+		}
 	echo "<br>";
 	}	
 ?>
